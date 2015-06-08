3604bf00e402c307ac095d86d797a6b86aeb0342
Kyle Lanmon <kyle.lanmon@gmail.com>
2015-06-08 02:03:55 -0500

    Update Ultra.c

--- src/Ultra.c
+++ src/Ultra.c
@@ -1,48 +1,9 @@
-/******************************************
- TODO:
-    -weather images
- ******************************************/
 #include <pebble.h>
-#define KEY_TEMPERATURE 0
-#define KEY_CONDITIONS 1
 
 static Window *window;
-
-static GBitmap *battery_image;
-static BitmapLayer *battery_image_layer;
-
-static GBitmap *bluetooth_image;
-static BitmapLayer *bluetooth_image_layer;
-
-static TextLayer *condition_layer;
-static TextLayer *temperature_layer;
-
-static TextLayer *time_layer;
-
-static TextLayer *date_layer;
-
-static GBitmap *dayOfWeek_image;
-static BitmapLayer *dayOfWeek_layer;
-
-GContext *time_ctx;
-
-const int BATTERY_LEVEL_RESOURCE_IDS[] = {
-    RESOURCE_ID_BATT0,
-    RESOURCE_ID_BATT1,
-    RESOURCE_ID_BATT2,
-    RESOURCE_ID_BATT3,
-    RESOURCE_ID_BATT4
-};
-
-const int WEEKDAY_RESOURCE_IDS[] = {
-    RESOURCE_ID_SUN,
-    RESOURCE_ID_MON,
-    RESOURCE_ID_TUE,
-    RESOURCE_ID_WED,
-    RESOURCE_ID_THU,
-    RESOURCE_ID_FRI,
-    RESOURCE_ID_SAT
-};
+static GBitmap *battery_image, *bluetooth_image, *dayOfWeek_image;
+static BitmapLayer *battery_image_layer, *bluetooth_image_layer, *dayOfWeek_layer;
+static TextLayer *charging_layer, *battery_layer, *time_layer, *date_layer;
 
 static void handle_time_and_date() {
     time_t temp = time(NULL); 
@@ -54,9 +15,31 @@ static void handle_time_and_date() {
     
     if (dayOfWeek_image)
         gbitmap_destroy(dayOfWeek_image);
-    
-    dayOfWeek_image = gbitmap_create_with_resource(WEEKDAY_RESOURCE_IDS[weekday]);
 
+    switch (weekday) {
+        case 0:
+            dayOfWeek_image = gbitmap_create_with_resource(RESOURCE_ID_SUN);
+            break;
+        case 1:
+            dayOfWeek_image = gbitmap_create_with_resource(RESOURCE_ID_MON);            
+            break;
+        case 2:
+            dayOfWeek_image = gbitmap_create_with_resource(RESOURCE_ID_TUE);
+            break;
+        case 3:
+            dayOfWeek_image = gbitmap_create_with_resource(RESOURCE_ID_WED);
+            break;
+        case 4:
+            dayOfWeek_image = gbitmap_create_with_resource(RESOURCE_ID_THU);
+            break;
+        case 5:
+            dayOfWeek_image = gbitmap_create_with_resource(RESOURCE_ID_FRI);
+            break;
+        case 6:
+            dayOfWeek_image = gbitmap_create_with_resource(RESOURCE_ID_SAT);
+            break;
+    }
+    
     if (!clock_is_24h_style())
         time_format = "%I:%M";
 
@@ -69,22 +52,34 @@ static void handle_time_and_date() {
 
 static void handle_battery() {
     BatteryChargeState charge_state = battery_state_service_peek();
+    static char charging_status[] = "xxxxxxxxxxx";
+    static char battery_status[] = "xxx%";
     
     if (battery_image)
         gbitmap_destroy(battery_image);
     
     if (charge_state.charge_percent > 75)
-        battery_image = gbitmap_create_with_resource(BATTERY_LEVEL_RESOURCE_IDS[0]);
+        battery_image = gbitmap_create_with_resource(RESOURCE_ID_BATT0);
     else if (charge_state.charge_percent <= 75 && charge_state.charge_percent > 50)
-        battery_image = gbitmap_create_with_resource(BATTERY_LEVEL_RESOURCE_IDS[1]);
+        battery_image = gbitmap_create_with_resource(RESOURCE_ID_BATT1);
     else if (charge_state.charge_percent <= 50 && charge_state.charge_percent > 25)
-        battery_image = gbitmap_create_with_resource(BATTERY_LEVEL_RESOURCE_IDS[2]);
-    else if (charge_state.charge_percent <= 25 && charge_state.charge_percent > 10)
-        battery_image = gbitmap_create_with_resource(BATTERY_LEVEL_RESOURCE_IDS[3]);
+        battery_image = gbitmap_create_with_resource(RESOURCE_ID_BATT2);
+    else if (charge_state.charge_percent <= 25 && charge_state.charge_percent > 0)
+        battery_image = gbitmap_create_with_resource(RESOURCE_ID_BATT3);
     else
-        battery_image = gbitmap_create_with_resource(BATTERY_LEVEL_RESOURCE_IDS[4]);
+        battery_image = gbitmap_create_with_resource(RESOURCE_ID_BATT4);
     
     bitmap_layer_set_bitmap(battery_image_layer, battery_image);
+    
+    if (charge_state.is_charging)
+        snprintf(charging_status, sizeof(charging_status), "charging");
+    else
+        snprintf(charging_status, sizeof(charging_status), "discharging");
+    
+    text_layer_set_text(charging_layer, charging_status);
+    
+    snprintf(battery_status, sizeof(battery_status), "%d%%", charge_state.charge_percent);
+    text_layer_set_text(battery_layer, battery_status);
 }
 
 void handle_bt(bool bt) {
@@ -103,19 +98,6 @@ void handle_minute_tick(struct tm *tick_time, TimeUnits units_changed) {
     handle_time_and_date();
     handle_battery();
     handle_bt(bluetooth_connection_service_peek());
-    
-    // Get weather update every 30 minutes
-    if(tick_time->tm_min % 30 == 0) {
-        // Begin dictionary
-        DictionaryIterator *iter;
-        app_message_outbox_begin(&iter);
-
-        // Add a key-value pair
-        dict_write_uint8(iter, 0, 0);
-
-        // Send the message!
-        app_message_outbox_send();
-    }
 }
 
 static void window_load(Window *window) {
@@ -144,23 +126,18 @@ static void window_load(Window *window) {
     battery_image_layer = bitmap_layer_create(GRect(5, 10, 144-5, 168-10));
     bitmap_layer_set_alignment(battery_image_layer, GAlignTopRight);
     bitmap_layer_set_bitmap(battery_image_layer, battery_image);
-    
-    //Load condition module
-    condition_layer = text_layer_create(GRect(5, 7, 144, 168));
-    text_layer_set_background_color(condition_layer, GColorClear);
-    text_layer_set_text_color(condition_layer, GColorWhite);
-    text_layer_set_text_alignment(condition_layer, GTextAlignmentCenter);
-    text_layer_set_text(condition_layer, "Loading");
-    
-    //Load temperature module
-    temperature_layer = text_layer_create(GRect(0, 20, 144, 168));
-    text_layer_set_text_alignment(temperature_layer, GTextAlignmentCenter);
-    text_layer_set_background_color(temperature_layer, GColorClear);
-    text_layer_set_text_color(temperature_layer, GColorWhite);
-    text_layer_set_font(temperature_layer, fonts_get_system_font(FONT_KEY_GOTHIC_28_BOLD));
-    text_layer_set_text(temperature_layer, "--- °F");
-    
-    //Load weather image module
+    charging_layer = text_layer_create(GRect(0, 10, 144, 168));
+    text_layer_set_text_color(charging_layer, GColorWhite);
+    text_layer_set_text(charging_layer, "xxxxxxxxxxx");
+    text_layer_set_background_color(charging_layer, GColorClear);
+    text_layer_set_text_alignment(charging_layer, GTextAlignmentCenter);
+    //text_layer_set_font(charging_layer, fonts_get_system_font(FONT_KEY_ROBOTO_CONDENSED_21));
+    battery_layer = text_layer_create(GRect(0, 25, 144, 168));
+    text_layer_set_text_color(battery_layer, GColorWhite);
+    text_layer_set_text(battery_layer, "xxxx");
+    text_layer_set_background_color(battery_layer, GColorClear);
+    text_layer_set_text_alignment(battery_layer, GTextAlignmentCenter);
+    text_layer_set_font(battery_layer, fonts_get_system_font(FONT_KEY_GOTHIC_28_BOLD));
     
     //Load bluetooth module
     bool bt = bluetooth_connection_service_peek();
@@ -177,8 +154,8 @@ static void window_load(Window *window) {
     layer_add_child(window_get_root_layer(window), bitmap_layer_get_layer(dayOfWeek_layer));
     layer_add_child(window_get_root_layer(window), text_layer_get_layer(date_layer));
     layer_add_child(window_get_root_layer(window), bitmap_layer_get_layer(battery_image_layer));
-    layer_add_child(window_get_root_layer(window), text_layer_get_layer(condition_layer));
-    layer_add_child(window_get_root_layer(window), text_layer_get_layer(temperature_layer));
+    layer_add_child(window_get_root_layer(window), text_layer_get_layer(charging_layer));
+    layer_add_child(window_get_root_layer(window), text_layer_get_layer(battery_layer));
     layer_add_child(window_get_root_layer(window), bitmap_layer_get_layer(bluetooth_image_layer));
     
     //Update time, date, bluetooth, and battery stats
@@ -201,60 +178,12 @@ static void window_unload(Window *window) {
     //Destroy battery module
     gbitmap_destroy(battery_image);
     bitmap_layer_destroy(battery_image_layer);
-    
-    //Destroy condition module
-    text_layer_destroy(condition_layer);
-    
-    //Destroy temperature module
-    text_layer_destroy(temperature_layer);
-    
-    //Destroy weather image module
+    text_layer_destroy(charging_layer);
+    text_layer_destroy(battery_layer);
     
     //Destroy bluetooth module
     gbitmap_destroy(bluetooth_image);
     bitmap_layer_destroy(bluetooth_image_layer);
-}
-
-static void inbox_received_callback(DictionaryIterator *iterator, void *context) {
-    // Store incoming information
-    static char temperature_buffer[8];
-    static char conditions_buffer[32];
-
-    // Read first item
-    Tuple *t = dict_read_first(iterator);
-    
-    // For all items
-    while(t != NULL) {
-        // Which key was received?
-        switch(t->key) {
-            case KEY_TEMPERATURE:
-                snprintf(temperature_buffer, sizeof(temperature_buffer), "%d °F", (int)t->value->int32);
-                text_layer_set_text(temperature_layer, temperature_buffer);
-                break;
-            case KEY_CONDITIONS:
-                snprintf(conditions_buffer, sizeof(conditions_buffer), "%s", t->value->cstring);
-                text_layer_set_text(condition_layer, conditions_buffer);
-                break;
-            default:
-                APP_LOG(APP_LOG_LEVEL_ERROR, "Key %d not recognized!", (int)t->key);
-                break;
-        }
-
-        // Look for next item
-        t = dict_read_next(iterator);
-    }
-}
-
-static void inbox_dropped_callback(AppMessageResult reason, void *context) {
-    APP_LOG(APP_LOG_LEVEL_ERROR, "Message dropped!");
-}
-
-static void outbox_failed_callback(DictionaryIterator *iterator, AppMessageResult reason, void *context) {
-    APP_LOG(APP_LOG_LEVEL_ERROR, "Outbox send failed!");
-}
-
-static void outbox_sent_callback(DictionaryIterator *iterator, void *context) {
-    APP_LOG(APP_LOG_LEVEL_INFO, "Outbox send success!");
 }
 
 static void init(void) {
@@ -269,13 +198,6 @@ static void init(void) {
     battery_state_service_subscribe(handle_battery);
     tick_timer_service_subscribe(MINUTE_UNIT, handle_minute_tick);
     bluetooth_connection_service_subscribe(handle_bt);
-    
-    app_message_register_inbox_received(inbox_received_callback);
-    app_message_register_inbox_dropped(inbox_dropped_callback);
-    app_message_register_outbox_failed(outbox_failed_callback);
-    app_message_register_outbox_sent(outbox_sent_callback);
-    
-    app_message_open(app_message_inbox_size_maximum(), app_message_outbox_size_maximum());
 }
 
 static void deinit(void) {
